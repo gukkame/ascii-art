eb05ba16e61dc05caa5bea31a6dad4792baf9614
gukka <gukka@DESKTOP-9PCH3KJ.localdomain>
2021-10-03 13:01:21 +0300

    ascii-art

--- main.go
+++ main.go
@@ -11,23 +11,27 @@ import (
 
 func main() {
 
-	if len(os.Args) == 2 {
+	if len(os.Args) > 1 {
 
 		incomingSentence := os.Args[1]
 
 		//Printing words so that \n marks the start of a new line
 		input_slice := strings.Split(incomingSentence, "\\n")
+
 		for _, word := range input_slice {
 
 			var ch, position rune
-			// var i int
 			var arr []int
 
 			for _, ch = range word {
 				position = (ch - 32) * 9
 				arr = append(arr, int(position)) // array of NUMBERS of first lines of input charachters
 			}
-			Printer(word, os.Args[2], arr)
+			if word == "" {
+				fmt.Println("")
+			} else {
+				Printer(word, os.Args[2], arr)
+			}
 		}
 	} else {
 		fmt.Println("Please enter more than 0 arguments")
@@ -40,6 +44,7 @@ func Printer(input, fontName string, originalArray []int) {
 			oneLineofOneLetter, _ := ReadLine(fontName, originalArray[i]+lineCounter)
 			fmt.Print(oneLineofOneLetter) //just print a bunch of same "height" lines of different letters in a row
 		}
+		fmt.Print(" ")
 		fmt.Print("\n") //and start a new line again
 	}
 }
@@ -47,7 +52,6 @@ func Printer(input, fontName string, originalArray []int) {
 func ReadLine(fontName string, startingLineEach int) (line string, err error) {
 
 	var lastLineOfScanner int
-
 	FontFile, err := os.Open(fontName) //open file
 	if err != nil {                    //check for errors
 		log.Fatal(err)
@@ -55,10 +59,8 @@ func ReadLine(fontName string, startingLineEach int) (line string, err error) {
 	defer FontFile.Close()                //close file
 	scanner := bufio.NewScanner(FontFile) // make a scaner machine aka not doing anything yet
 	for scanner.Scan() {                  //loop through lines until you reached the wished line aka start machine
-
 		if lastLineOfScanner == startingLineEach {
 			return scanner.Text(), scanner.Err() //scanner.Text() is where you store the result of scanner you can print it aswell if you make it equal to a value
-
 		}
 		lastLineOfScanner++
 	}
