e75c70b9fef638fe67e33729753bfb25d3cc3917
gukka <gukka@DESKTOP-9PCH3KJ.localdomain>
2021-10-03 12:06:42 +0300

    art

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	if len(os.Args) == 2 {
+
+		incomingSentence := os.Args[1]
+
+		//Printing words so that \n marks the start of a new line
+		input_slice := strings.Split(incomingSentence, "\\n")
+		for _, word := range input_slice {
+
+			var ch, position rune
+			// var i int
+			var arr []int
+
+			for _, ch = range word {
+				position = (ch - 32) * 9
+				arr = append(arr, int(position)) // array of NUMBERS of first lines of input charachters
+			}
+			Printer(word, os.Args[2], arr)
+		}
+	} else {
+		fmt.Println("Please enter more than 0 arguments")
+	}
+}
+
+func Printer(input, fontName string, originalArray []int) {
+	for lineCounter := 1; lineCounter < 9; lineCounter++ { //loop through the heigth of the ascii characters
+		for i, _ := range originalArray {
+			oneLineofOneLetter, _ := ReadLine(fontName, originalArray[i]+lineCounter)
+			fmt.Print(oneLineofOneLetter) //just print a bunch of same "height" lines of different letters in a row
+		}
+		fmt.Print("\n") //and start a new line again
+	}
+}
+
+func ReadLine(fontName string, startingLineEach int) (line string, err error) {
+
+	var lastLineOfScanner int
+
+	FontFile, err := os.Open(fontName) //open file
+	if err != nil {                    //check for errors
+		log.Fatal(err)
+	}
+	defer FontFile.Close()                //close file
+	scanner := bufio.NewScanner(FontFile) // make a scaner machine aka not doing anything yet
+	for scanner.Scan() {                  //loop through lines until you reached the wished line aka start machine
+
+		if lastLineOfScanner == startingLineEach {
+			return scanner.Text(), scanner.Err() //scanner.Text() is where you store the result of scanner you can print it aswell if you make it equal to a value
+
+		}
+		lastLineOfScanner++
+	}
+	return line, io.EOF // it will return the line when the scanner mathces reading to the input line number aka gets to it
+}
