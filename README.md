0125e53f3dd1e5acfbdc0b7570f29d057b572c0b
gukka <guka245@gmail.com>
2022-11-01 11:17:42 +0200

    ascii art

---
+++ README.md
@@ -0,0 +1,31 @@
+Objectives//////////////////
+Ascii-art is a program which consists in receiving a string as an argument and outputting the string in a graphic representation using ASCII. Time to write big.
+
+What we mean by a graphic representation using ASCII, is to write the string received using ASCII characters, as you can see in the example below:
+
+This project should handle an input with numbers, letters, spaces, special characters and \n.
+
+
+Instructions //////////////////
+Your project must be written in Go.
+
+The code must respect the good practices.
+
+It is recommended that the code present a test file.
+
+Some banner files with a specific graphical template representation using ASCII will be given. The files are formatted in a way that is not necessary to change them.
+
+Banner Format////////////////////
+Each character has a height of 8 lines.
+Characters are separated by a new line \n.
+Here is an example  :
+
+student$ go run . "Hello\n" | cat -e
+ _    _          _   _          $
+| |  | |        | | | |         $
+| |__| |   ___  | | | |   ___   $
+|  __  |  / _ \ | | | |  / _ \  $
+| |  | | |  __/ | | | | | (_) | $
+|_|  |_|  \___| |_| |_|  \___/  $
+                                $
+                                $
--- main.go
+++ main.go
@@ -1,36 +1,38 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-	"log"
-	"os"
-	"strings"
+    "bufio"
+    "fmt"
+    "os"
+    "strings"
+    "io"
+    "log"
 )
-
 func main() {
 
-	if len(os.Args) > 1 {
+	if len(os.Args)  > 1 {
 
 		incomingSentence := os.Args[1]
+		
+		
+
 
 		//Printing words so that \n marks the start of a new line
-		input_slice := strings.Split(incomingSentence, "\\n")
-
+		input_slice := strings.Split(incomingSentence,"\\n")
+		
 		for _, word := range input_slice {
 
-			var ch, position rune
-			var arr []int
-
+			var ch, position rune	
+			var arr []int 
+		
 			for _, ch = range word {
 				position = (ch - 32) * 9
 				arr = append(arr, int(position)) // array of NUMBERS of first lines of input charachters
 			}
-			if word == "" {
+			if word == ""{
 				fmt.Println("")
-			} else {
-				Printer(word, os.Args[2], arr)
+			}else{
+			Printer(word, "standard.txt", arr)
 			}
 		}
 	} else {
@@ -38,29 +40,33 @@ func main() {
 	}
 }
 
-func Printer(input, fontName string, originalArray []int) {
+func Printer(input, fontName string, originalArray[] int) {
 	for lineCounter := 1; lineCounter < 9; lineCounter++ { //loop through the heigth of the ascii characters
-		for i, _ := range originalArray {
+        for i,_ := range originalArray{
 			oneLineofOneLetter, _ := ReadLine(fontName, originalArray[i]+lineCounter)
-			fmt.Print(oneLineofOneLetter) //just print a bunch of same "height" lines of different letters in a row
+			fmt.Print(oneLineofOneLetter) //just print a bunch of same "height" lines of different letters in a row 
 		}
 		fmt.Print(" ")
 		fmt.Print("\n") //and start a new line again
 	}
 }
 
+
+
 func ReadLine(fontName string, startingLineEach int) (line string, err error) {
 
-	var lastLineOfScanner int
-	FontFile, err := os.Open(fontName) //open file
-	if err != nil {                    //check for errors
+    var lastLineOfScanner int
+    FontFile, err := os.Open(fontName) //open file
+	if err != nil { //check for errors
 		log.Fatal(err)
 	}
-	defer FontFile.Close()                //close file
+	defer FontFile.Close() //close file
 	scanner := bufio.NewScanner(FontFile) // make a scaner machine aka not doing anything yet
-	for scanner.Scan() {                  //loop through lines until you reached the wished line aka start machine
-		if lastLineOfScanner == startingLineEach {
-			return scanner.Text(), scanner.Err() //scanner.Text() is where you store the result of scanner you can print it aswell if you make it equal to a value
+
+	for scanner.Scan() { //loop through lines until you reached the wished line aka start machine 
+
+        if lastLineOfScanner == startingLineEach {
+			return scanner.Text(), scanner.Err() //scanner.Text() is where you store the result of scanner you can print it aswell if you make it equal to a value         
 		}
 		lastLineOfScanner++
 	}
